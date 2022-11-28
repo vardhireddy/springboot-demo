2e2e6cd730688849b07d13df18c1e45f747531fe
vardhireddy <krishna.vardhireddy@gmail.com>
2022-11-28 13:44:32 +0530

    AB#10 - add the docker file and update CI pipeline

--- src/main/java/com/example/azuredemo/AzuredemoApplication.java
+++ src/main/java/com/example/azuredemo/AzuredemoApplication.java
@@ -2,10 +2,16 @@ package com.example.azuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzuredemoApplication {
-
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AzuredemoApplication.class, args);
 	}
